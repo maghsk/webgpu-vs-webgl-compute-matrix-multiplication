4099af5b01ac9248d9caa918ed0b5cc1db189b10
Obsido Han <maghsk2017@gmail.com>
2022-10-17 14:23:37 +0800

    fix grammar on chrome canary 109.0.5363.0

--- webgpu_matrix.js
+++ webgpu_matrix.js
@@ -106,17 +106,17 @@ async function multiplyWebGPU(matrixSize, device) {
 
     const shaderModule = device.createShaderModule({
         code: `
-      [[block]] struct Matrix {
-        size : vec2<f32>;
-        numbers: array<f32>;
+      struct Matrix {
+        size : vec2<f32>,
+        numbers: array<f32>,
       };
       
-      [[group(0), binding(0)]] var<storage, read> firstMatrix : Matrix;
-      [[group(0), binding(1)]] var<storage, read> secondMatrix : Matrix;
-      [[group(0), binding(2)]] var<storage, write> resultMatrix : Matrix;
+      @group(0) @binding(0) var<storage, read> firstMatrix : Matrix;
+      @group(0) @binding(1) var<storage, read> secondMatrix : Matrix;
+      @group(0) @binding(2) var<storage, read_write> resultMatrix : Matrix;
       
-      [[stage(compute), workgroup_size(8, 8)]]
-      fn main([[builtin(global_invocation_id)]] global_id : vec3<u32>) {
+      @compute @workgroup_size(8, 8)
+      fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
         // Guard against out-of-bounds work group sizes.
         if (global_id.x >= u32(firstMatrix.size.x) || global_id.y >= u32(secondMatrix.size.y)) {
           return;
@@ -159,8 +159,8 @@ async function multiplyWebGPU(matrixSize, device) {
     passEncoder.setBindGroup(0, bindGroup);
     const x = Math.min(Math.ceil(firstMatrix[0] / 8), 256); // X dimension of the grid of workgroups to dispatch.
     const y = Math.min(Math.ceil(secondMatrix[1] / 8), 256); // Y dimension of the grid of workgroups to dispatch.
-    passEncoder.dispatch(x, y);
-    passEncoder.endPass();
+    passEncoder.dispatchWorkgroups(x, y);
+    passEncoder.end();
 
     // Get a GPU buffer for reading in an unmapped state.
     const gpuReadBuffer = device.createBuffer({
